43fab380ab30e3f47f9bd40f5721e475cdfed045
Jan Wrobel <wrr@mixedbit.org>
2022-10-09 21:54:13 +0200

    Add missing tests to __init__.

--- wwwhisper_auth/tests/__init__.py
+++ wwwhisper_auth/tests/__init__.py
@@ -1,8 +1,10 @@
 """Tests wwwhisper_auth package."""
 
-from wwwhisper_auth.tests.tests_models import *
+from wwwhisper_auth.tests.tests_backend import *
 from wwwhisper_auth.tests.tests_http import *
+from wwwhisper_auth.tests.tests_login_token import *
 from wwwhisper_auth.tests.tests_middleware import *
+from wwwhisper_auth.tests.tests_models import *
 from wwwhisper_auth.tests.tests_site_cache import *
 from wwwhisper_auth.tests.tests_url_utils import *
 from wwwhisper_auth.tests.tests_views import *
