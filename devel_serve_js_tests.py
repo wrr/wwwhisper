dffcc917f166d0e9167453c62267a6ee7a44d7d9
Jan Wrobel <wrr@mixedbit.org>
2023-05-03 18:57:16 +0200

    A script to easily server JS unit tests in devel.

---
+++ devel_serve_js_tests.py
@@ -0,0 +1,16 @@
+import http.server
+import socketserver
+
+PORT = 8000
+
+class Handler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory='www_static', **kwargs)
+
+
+# After this script is started, JS unit test runner is available at
+# http://localhost:8000/js_tests/index.html
+
+with socketserver.TCPServer(('127.0.0.1', PORT), Handler) as httpd:
+    print(f'Serving static files at port {PORT}')
+    httpd.serve_forever()
