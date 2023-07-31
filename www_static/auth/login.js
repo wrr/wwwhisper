1fb04032c2f553d0fb772ef4a5bf2311a5860704
Jan Wrobel <wrr@mixedbit.org>
2023-07-31 13:36:10 +0200

    Drop jquery dependency from login.html.

--- www_static/auth/login.js
+++ www_static/auth/login.js
@@ -5,34 +5,42 @@
   'use strict';
   var net = new wwwhisper.Net();
 
+  function getById(id) {
+    return document.getElementById(id);
+  }
+
   /**
    * Requests a login token to be send to the user provided email
    * address.
    */
   function login(email) {
-    $('.entered-email').text(email);
+    var elements = document.getElementsByClassName('entered-email'), i;
+    for (i = 0; i < elements.length; ++i) {
+      elements[i].innerText = email;
+    }
     net.ajax('POST', '/wwwhisper/auth/api/send-token/',
              {
                'email' : email,
                'path': window.location.pathname
              },
              function() {
-               $('#intro').addClass('hide');
-               $('#login-form').addClass('hide');
-               $('#token-send-success').removeClass('hide');
+               getById('intro').classList.add('hide');
+               getById('login-form').classList.add('hide');
+               getById('token-send-success').classList.remove('hide');
              },
              function(errorMessage, errorStatus) {
-               $('#token-send-error-message').text(errorMessage);
-               $('#token-send-error').removeClass('hide');
+               getById('token-send-error-message').innerText = errorMessage;
+               getById('token-send-error').classList.remove('hide');
              });
   }
 
-  $('#login-required').removeClass('hide');
-  $('#login-form').removeClass('hide');
+  getById('login-required').classList.remove('hide');
+  getById('login-form').classList.remove('hide');
 
-  $('#login-form').submit(function() {
-    var email = $.trim($('#email').val());
-    $('#token-send-error').addClass('hide');
+  getById('login-form').addEventListener('submit', function(event) {
+    var email = getById('email').value.trim();
+    event.preventDefault();
+    getById('token-send-error').classList.add('hide');
     if (email.length !== 0) {
       login(email);
     }
