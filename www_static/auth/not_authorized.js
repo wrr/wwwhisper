3a32c2c433a49733f3a6408043328d1fa93051c6
Jan Wrobel <wrr@mixedbit.org>
2023-07-31 12:23:21 +0200

    Drop jquery dependency from not_authorized.html.

--- www_static/auth/not_authorized.js
+++ www_static/auth/not_authorized.js
@@ -4,13 +4,13 @@
 (function () {
   'use strict';
 
-  $('#logout').click(function() {
+  function logout() {
     var net = new wwwhisper.Net();
-
     net.ajax('POST', '/wwwhisper/auth/api/logout/', {}, function() {
       window.top.location = '/wwwhisper/auth/goodbye.html';
     });
     return false;
-  });
+  }
 
+  document.getElementById('logout').addEventListener('click', logout);
 }());
