f95fae695da8757248cbe4c872f0334bb786ed93
Jan Wrobel <jan@mixedbit.org>
2025-06-24 14:13:03 +0200

    Bump version

--- internal/proxy/proxy.go
+++ internal/proxy/proxy.go
@@ -32,7 +32,7 @@ type Config struct {
 	LogLevel     slog.Level
 }
 
-const Version string = "1.0.4"
+const Version string = "1.0.5"
 const Client string = "go-" + Version
 const overlayToInject = `
 <script src="/wwwhisper/auth/iframe.js"></script>
